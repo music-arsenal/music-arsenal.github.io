0a74ab6725b87bff024f0547f028d6bcd0aa4728
music-arsenal <zion.pirs@mail.ru>
2017-05-06 14:12:26 +0300

    Добавляет активный класс элементам меню navigation

--- js/common.js
+++ js/common.js
@@ -2,6 +2,10 @@
 * основной js файл
 **/
 $(document).ready(function () {
+    var Navigation = $('.navigation');
+    Navigation.find('ul li').click(function () {
+        Navigation.find('ul li').removeClass('active');
+       $(this).addClass('active');
+    });
 
-    alert("Вы загрузили страницу!");
 });
