e6c6fdcb708ec07a724c721d08388d8968cc5871
music-arsenal <zion.pirs@mail.ru>
2017-05-10 19:52:35 +0300

    исправил выпадающее меню

--- js/common.js
+++ js/common.js
@@ -4,14 +4,19 @@
 $(document).ready(function () {
     var Menu = $('.menu');
     var SubMenu = $('.submenu');
+    var LastMenu = $('.last-menu');
     Menu.find('li').click(function () {
         Menu.find('li').removeClass('active');
        $(this).addClass('active');
-        SubMenu.find('li').click(function () {
+    });
+      SubMenu.find('li').click(function () {
            SubMenu.find('li').removeClass('activ');
             $(this).addClass('activ');
         });
-    });
+      LastMenu.find('li').click(function() {
+         LastMenu.find('li').removeClass('activi');
+          $(this).addClass('activi');
+      });
 
     $('.one-time').slick({
       infinite: true,
